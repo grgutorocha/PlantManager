481e3d490808cf2dffcaaa227c49b8a8c5c0370b
Augusto Rocha <grgutorocha@gmail.com>
2022-11-10 22:41:45 -0300

    Initial Commit

---
+++ PlantManager/PlantManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantManagerApp.swift
+//  PlantManager
+//
+//  Created by Augusto Rocha on 10/11/22.
+//
+
+import SwiftUI
+
+@main
+struct PlantManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
